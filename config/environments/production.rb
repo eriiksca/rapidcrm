e1f0ad9eacc99d254a774d5427c57821a062d5d9
Erika Cornejo <erika.cornejoa@gmail.com>
2016-07-20 15:04:02 +0000

    added initializer for precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-config.assets.initialize_on_precpmpile = false
+config.assets.initialize_on_precompile = false
